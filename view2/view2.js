59055aa6e94e80f6f54fb1ffa52e19cb1d8339b6
wilso_000 <wilson_novido@yahoo.com>
2014-11-01 12:24:11 -0400

    Added Formula 1 Feeder

--- view2/view2.js
+++ view2/view2.js
@@ -9,6 +9,18 @@ angular.module('myApp.view2', ['ngRoute'])
   });
 }])
 
-.controller('View2Ctrl', [function() {
+.controller('View2Ctrl', function($scope) {
+      $scope.bmi = 0;
+      $scope.heightFt = 0;
+      $scope.heightInch = 0;
+      $scope.weightLb = 0;
 
-}]);
+      $scope.calcBMI = function() {
+        if ($scope.heightFt === null || $scope.heightInch === null || $scope.weightLb === null)
+          $scope.bmi = 0;
+        else
+          $scope.bmi = $scope.weightLb/Math.pow($scope.heightFt * 12 + $scope.heightInch,2)*703;
+      };
+});
+
+
