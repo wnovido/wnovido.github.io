3b3d696ec08af69db7e37e513f03a4b742413126
wilso_000 <wilson_novido@yahoo.com>
2014-10-31 22:02:35 -0400

    Added checking of null values.

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ app.controller('BMIController',function($scope) {
         $scope.weightLb = 0;
 
         $scope.calcBMI = function() {
-            $scope.bmi = $scope.weightLb/Math.pow($scope.heightFt * 12 + $scope.heightInch,2)*703;
+        	if ($scope.heightFt === null || $scope.heightInch === null || $scope.weightLb === null)
+        		$scope.bmi = 0;
+        	else
+            	$scope.bmi = $scope.weightLb/Math.pow($scope.heightFt * 12 + $scope.heightInch,2)*703;
         };
 });
